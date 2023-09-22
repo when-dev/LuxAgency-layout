b51f8af49f56ec52e8780d19f4eb0947ffbad618
Egor Gukov <108114245+when-dev@users.noreply.github.com>
2023-09-22 21:53:18 +0300

    Add styles to the header

--- src/App.js
+++ src/App.js
@@ -1,7 +1,43 @@
+import logo from "./assets/image/logo.svg";
+import callbtn from "./assets/image/call.svg";
+import "./assets/css/style.css";
 
 function App() {
   return (
     <div className="App">
+      <header className="header">
+        <div className="header__inner">
+          <div className="container">
+            <img className="logo" src={logo} alt="logo" />
+            <nav className="header__nav">
+              <ul>
+                <li>
+                  <a href="#!">Главная</a>
+                </li>
+                <li>
+                  <a href="#!">О компании</a>
+                </li>
+                <li>
+                  <a href="#!">Услуги</a>
+                </li>
+                <li>
+                  <a href="#!">Проекты</a>
+                </li>
+                <li>
+                  <a href="#!">Отзывы</a>
+                </li>
+              </ul>
+            </nav>
+            <img className="callbtn" src={callbtn} alt="callbtn" />
+          </div>
+        </div>
+          <h1>агентство недвижимости</h1>
+          <h2>Лучшие идеи – Лучшее решение – Лучший результат</h2>
+          <div className="center-content">
+            <button className="order-btn">заказать консультацию</button>
+          </div>
+      </header>
+
       
     </div>
   );
