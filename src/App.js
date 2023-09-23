d29ebeef83bb3f2210f86fa43286d7296de98a22
Egor Gukov <108114245+when-dev@users.noreply.github.com>
2023-09-23 10:21:58 +0300

    add an index.js with image export and complete the our_team section

--- src/App.js
+++ src/App.js
@@ -1,3 +1,17 @@
+import {
+  maxWiwi,
+  wellSofi,
+  ruslan,
+  anna,
+  igorek,
+  antosha,
+  vitaminka,
+  muslim,
+  antonodzhi,
+  vladik,
+  andrusha,
+  platina,
+} from "./assets/image/index.js";
 import logo from "./assets/image/logo.svg";
 import callbtn from "./assets/image/call.svg";
 import "./assets/css/style.css";
@@ -31,14 +45,77 @@ function App() {
             <img className="callbtn" src={callbtn} alt="callbtn" />
           </div>
         </div>
-          <h1>агентство недвижимости</h1>
-          <h2>Лучшие идеи – Лучшее решение – Лучший результат</h2>
-          <div className="center-content">
-            <button className="order-btn">заказать консультацию</button>
-          </div>
+        <h1>агентство недвижимости</h1>
+        <h2>Лучшие идеи – Лучшее решение – Лучший результат</h2>
+        <div className="center-content">
+          <button className="order-btn">заказать консультацию</button>
+        </div>
       </header>
 
-      
+      <section className="our_team">
+        <div className="team__container">
+          <h2 className="team-title">
+            Наша <br /> команда
+          </h2>
+          <p className="team-description">
+            Команда профессионалов, которая работает над тем, чтобы предоставить <br />
+            вам лучший сервис и креативные решения. Мы рады реализовать самые <br />
+            сложные проекты и предоставить вам недвижимость вашей мечты!
+          </p>
+        </div>
+        <div className="container">
+          <div className="team-grid">
+            <div className="team-member">
+              <img src={maxWiwi} alt="maxWiwi" />
+              <p className="team-member-name">Max Wiwi</p>
+            </div>
+            <div className="team-member">
+              <img src={wellSofi} alt="wellSofi" />
+              <p className="team-member-name">Well Sofi</p>
+            </div>
+            <div className="team-member">
+              <img src={ruslan} alt="ruslan" />
+              <p className="team-member-name">Руслан</p>
+            </div>
+            <div className="team-member">
+              <img src={anna} alt="anna" />
+              <p className="team-member-name">Анна</p>
+            </div>
+            <div className="team-member">
+              <img src={igorek} alt="igorek" />
+              <p className="team-member-name">Игорёк</p>
+            </div>
+            <div className="team-member">
+              <img src={antosha} alt="antosha" />
+              <p className="team-member-name">Антоша</p>
+            </div>
+            <div className="team-member">
+              <img src={vitaminka} alt="Витаминка" />
+              <p className="team-member-name">Витаминка</p>
+            </div>
+            <div className="team-member">
+              <img src={muslim} alt="Муслим" />
+              <p className="team-member-name">Муслим</p>
+            </div>
+            <div className="team-member">
+              <img src={antonodzhi} alt="Антоноджи" />
+              <p className="team-member-name">Антоноджи</p>
+            </div>
+            <div className="team-member">
+              <img src={vladik} alt="Владик" />
+              <p className="team-member-name">Владик</p>
+            </div>
+            <div className="team-member">
+              <img src={andrusha} alt="Андрюша" />
+              <p className="team-member-name">Андрюша</p>
+            </div>
+            <div className="team-member">
+              <img src={platina} alt="Платина" />
+              <p className="team-member-name">Платина</p>
+            </div>
+          </div>
+        </div>
+      </section>
     </div>
   );
 }
---
+++ src/assets/image/index.js
@@ -0,0 +1,29 @@
+import maxWiwi from "./our_team/max-wiwi.png";
+import wellSofi from "./our_team/well-sofi.png";
+import ruslan from "./our_team/ruslan.png";
+import anna from "./our_team/anna.png";
+import igorek from "./our_team/igorek.png";
+import antosha from "./our_team/antosha.png";
+import vitaminka from "./our_team/vitaminka.png";
+import muslim from "./our_team/muslim.png";
+import antonodzhi from "./our_team/antonodzhi.png";
+import vladik from "./our_team/vladik.png";
+import andrusha from "./our_team/andrusha.png";
+import platina from "./our_team/platina.png";
+
+
+// export default maxWiwi;
+export {
+  maxWiwi,
+  wellSofi,
+  ruslan,
+  anna,
+  igorek,
+  antosha,
+  vitaminka,
+  muslim,
+  antonodzhi,
+  vladik,
+  andrusha,
+  platina,
+};
