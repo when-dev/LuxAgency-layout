17c02bfb04a9516ba98c5b67c02780caccc3f212
Egor Gukov <108114245+when-dev@users.noreply.github.com>
2023-09-25 21:45:18 +0300

    minor fix

--- src/components/modal/Modal.jsx
+++ src/components/modal/Modal.jsx
@@ -1,13 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import "./modal.css";
 import InputMask from "react-input-mask";
-
+import axios from "axios";
 
 const ModalComponent = ({ isOpen, onClose }) => {
+  const [name, setName] = useState("");
+  const [phone, setPhone] = useState("");
+
   const closeModalOnOutsideClick = (e) => {
     if (isOpen && e.target.classList.contains("modal")) {
       onClose();
     }
+  };
+
+  const handleSubmit = () => {
+    axios
+      .post("https://localhost:3000/create_lead", {
+        lead_type: "Перезвон", 
+        contact_info: "89996295430",
+      })
+      .then((response) => {
+        console.log("Успешно отправлено", response.data);
+      })
+      .catch((error) => {
+        console.error("Произошла ошибка при отправке", error);
+      });
+
+    onClose();
   };
 
   return isOpen ? (
@@ -22,6 +41,8 @@ const ModalComponent = ({ isOpen, onClose }) => {
             placeholder="Имя"
             required
             pattern="[A-Za-zА-Яа-яЁё]*"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
           />
         </div>
         <div>
@@ -32,9 +53,11 @@ const ModalComponent = ({ isOpen, onClose }) => {
             className="phone-input-modal"
             placeholder="Телефон"
             required
+            value={phone}
+            onChange={(e) => setPhone(e.target.value)}
           />
         </div>
-        <button className="submit-btn">Отправить</button>
+        <button className="submit-btn" onClick={handleSubmit}>Отправить</button>
         <div>
           <p className="modal-telegram">Или свяжитесь с нами в Telegram:</p>
           <a href="#!" target="_blank" rel="noopener noreferrer">
